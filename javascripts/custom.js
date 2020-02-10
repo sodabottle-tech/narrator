3ef6055348593c2272a2e398bea17c05ffb828fc
Vaibhav Jain <vaibhav.j@browserstack.com>
2020-02-10 11:45:41 +0530

    sentry

---
+++ javascripts/custom.js
@@ -0,0 +1,31 @@
+function hello(){
+	console.log('hello');
+
+	console.log('how');
+
+	console.log('are');
+
+	console.log('you');
+
+	console.log(akj);
+}
+
+
+setTimeout(function(){
+	$('.logo').click(function(){
+		hello();
+	});
+});
+
+function hello1(){
+	console.log('hello');
+
+	console.log('how');
+
+	console.log('are');
+
+	console.log('you');
+
+	console.log('now');
+}
+
