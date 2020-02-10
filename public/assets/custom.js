fdf8d8720630d72cf1ad376ffe109d0ed4fef458
Vaibhav Jain <vaibhav.j@browserstack.com>
2020-02-10 12:59:12 +0530

    remove sourcemap

--- public/assets/custom.js
+++ public/assets/custom.js
@@ -1,2 +1,1 @@
 function hello(){console.log("hello"),console.log("how"),console.log("are"),console.log("you"),console.log(akj)}function hello1(){console.log("hello"),console.log("how"),console.log("are"),console.log("you"),console.log("now")}setTimeout(function(){$(".logo").click(function(){hello()})});
-//# sourceMappingURL=custom.js.map
