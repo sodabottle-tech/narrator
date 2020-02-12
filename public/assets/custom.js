453cbd477a3530dde30ea02c75cc6ac0ddd047bc
Vaibhav Jain <vaibhav.j@browserstack.com>
2020-02-12 18:12:56 +0530

    updates

--- public/assets/custom.js
+++ public/assets/custom.js
@@ -1,2 +1,2 @@
 function hello(){console.log("hello"),console.log("how"),console.log("are"),console.log("you"),console.log(puneet1234)}function hello1(){console.log("hello"),console.log("how"),console.log("are"),console.log("you"),console.log("now")}setTimeout(function(){$(".logo").click(function(){hello()})});
-//# sourceMappingURL=https://test-sentry-poc.s3.amazonaws.com/custom.js.map
+//# sourceMappingURL=custom.js.map
